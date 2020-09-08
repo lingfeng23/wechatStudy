e2cc651d7e19dc3a6beccaf70480dc543fb039d6
lingfeng23 <15131257162@163.com>
2020-09-08 22:45:51 +0800

    初始化项目

---
+++ src/App.java
@@ -0,0 +1,13 @@
+package src;
+
+/**
+ * @author malf
+ * @Description 测试
+ * @project wechatStudy
+ * @since 2020/9/8
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.printf("床前明月光，疑是地上霜。");
+	}
+}
