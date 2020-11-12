9227ca971099133077fa806a70688ed52e4af168
lingfeng23 <15131257162@163.com>
2020-11-12 16:59:58 +0800

    springboot init

---
+++ springboot/src/test/java/com/malf/SpringbootApplicationTests.java
@@ -0,0 +1,18 @@
+package com.malf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	void test() {
+		assert 1 == 1;
+	}
+
+}
